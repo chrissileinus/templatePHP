c5227170daaefc620ed4b85048995a49f283bc0f
chrissileinus <chrissileinus@googlemail.com>
2021-11-03 12:21:24 +0000

    fdshgs

--- src/Str.php
+++ src/Str.php
@@ -1,4 +1,11 @@
 <?php
+/*
+ * Created on Wed Nov 03 2021
+ *
+ * Copyright (c) 2021 Christian Backus (Chrissileinus)
+ *
+ * For the full copyright and license information, please view the LICENSE file that was distributed with this source code.
+ */
 
 namespace Chrissileinus\Template;
 
