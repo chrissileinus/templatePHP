1a6b4cc8dad7f81b3daea2068ede501177f87055
chrissileinus <chrissileinus@googlemail.com>
2021-11-03 09:20:52 +0000

    sdg

--- src/Str.php
+++ src/Str.php
@@ -7,8 +7,10 @@ class Str
   public static $left = '{';
   public static $right = '}';
 
-  public static function replace(string $template, array $replacements): string
+  public static function replace(string $template, array ...$replacements): string
   {
+    $replacements = array_replace_recursive([], ...$replacements);
+
     $result = $template;
 
     foreach (new nestedKeyIterator(new recursiveArrayOnlyIterator($replacements)) as $key => $value) {
@@ -18,24 +20,36 @@ class Str
     return $result;
   }
 
-  public static function replaceF(string $template, array $replacements): string
+  public static function replaceF(string $template, array ...$replacements): string
   {
     //Performance: if there are no '%' fallback to self::string
     if (strstr($template, '%') == false && strstr($template, '&') == false) {
-      return self::replace($template, $replacements);
+      return self::replace($template, ...$replacements);
     }
 
     $result = $template;
 
-    foreach (new nestedKeyIterator(new recursiveArrayOnlyIterator($replacements)) as $key => $value) {
+    $replacements = iterator_to_array(new nestedKeyIterator(new recursiveArrayOnlyIterator(array_replace_recursive([], ...$replacements))));
+
+    foreach ($replacements as $key => $value) {
       $pattern = "/" . self::$left . $key . "(%[^" . self::$right . "&]+)?" . "(?:&([^" . self::$right . "]+))?" . self::$right . "/";
       preg_match_all($pattern, $template, $matches);
 
-      $substs = array_map(function ($match, $ansi) use ($value) {
+      $substs = array_map(function ($match, $ansi) use ($value, $replacements) {
         $tmp = $match !== '' ? sprintf($match, $value) : $value;
 
+        $ansis = [];
         if ($ansi) {
-          $tmp = \Chrissileinus\Ansi\Text::inject($tmp, ...explode(',', $ansi));
+          foreach (explode(',', $ansi) as $code) {
+            if (array_key_exists($code, $replacements)) {
+              foreach (explode(',', $replacements[$code]) as $_code) {
+                $ansis[] = $_code;
+              }
+              continue;
+            }
+            $ansis[] = $code;
+          }
+          $tmp = \Chrissileinus\Ansi\Text::inject($tmp, ...$ansis);
         }
 
         return $tmp;
@@ -49,13 +63,20 @@ class Str
       $pattern = "/" . self::$left . "([^&]+)(?:&([^" . self::$right . "]+))" . self::$right . "/";
       preg_match_all($pattern, $result, $matches);
 
-      var_dump($matches);
-
-      $substs = array_map(function ($value, $ansi) {
+      $substs = array_map(function ($value, $ansi) use ($replacements) {
         $tmp = $value;
 
         if ($ansi) {
-          $tmp = \Chrissileinus\Ansi\Text::inject($tmp, ...explode(',', $ansi));
+          foreach (explode(',', $ansi) as $code) {
+            if (array_key_exists($code, $replacements)) {
+              foreach (explode(',', $replacements[$code]) as $_code) {
+                $ansis[] = $_code;
+              }
+              continue;
+            }
+            $ansis[] = $code;
+          }
+          $tmp = \Chrissileinus\Ansi\Text::inject($tmp, ...$ansis);
         }
 
         return $tmp;
--- test/string.php
+++ test/string.php
@@ -2,14 +2,27 @@
 
 include __DIR__ . '/../vendor/autoload.php';
 
-$template = "my name is {name&f_yellow} and my arge is {age&f_blue} {[{weight%6.2f}]&bold,f_magenta}";
+$template = "my name is {user.name&user.color} and my arge is {age&f_blue} {[{weight%6.2f}]&test.style}";
 
-$replacemants = [
-  'name' => "chris",
+$replacements = [
+  'user' => [
+    'name' => "chris"
+  ],
   'age' => 21,
-  'weight' => 83.4
+  'weight' => 83.4,
+  'test' => [
+    'style' => "bold,f_magenta"
+  ]
 ];
 
-$result = Chrissileinus\Template\Str::replaceF($template, $replacemants);
+$result = Chrissileinus\Template\Str::replaceF(
+  $template,
+  $replacements,
+  [
+    'user' => [
+      'color' => "f_yellow"
+    ]
+  ]
+);
 
 echo $result . PHP_EOL;
